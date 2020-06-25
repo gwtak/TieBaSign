169e8c0c248f3bb736cdaa2561c7c240391d20ce
ghosx <ghosx@qq.com>
2020-06-25 10:39:53 +0800

    github action for free

---
+++ README.md
@@ -0,0 +1,18 @@
+# 贴吧签到Github Action版
+
+## 使用说明
+
+Fork 本仓库，然后点击你的仓库右上角的 Settings，找到 Secrets 这一项，添加一个秘密环境变量。
+
+![捕获.PNG](https://i.loli.net/2020/06/25/yxcj6rBtufhPS78.png)
+
+其中 `BDUSS` 存放你的 BDUSS 。支持同时添加多个帐户，BDUSS 之间用半角逗号 `#` 隔开即可。
+
+设置好环境变量后点击你的仓库上方的 Actions 选项，会打开一个如下的页面，点击 `I understand...` 按钮确认在 Fork 的仓库上启用 GitHub Actions 。
+
+## 讨论群
+
+TG： https://t.me/tiebasign
+
+qq群： 818794879
+
---
+++ main.py
@@ -0,0 +1,176 @@
+# -*- coding:utf-8 -*-
+import os
+import requests
+import hashlib
+import time
+import copy
+import logging
+
+# API_URL
+LIKIE_URL = "http://c.tieba.baidu.com/c/f/forum/like"
+TBS_URL = "http://tieba.baidu.com/dc/common/tbs"
+SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign"
+
+HEADERS = {
+    'Host': 'tieba.baidu.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+}
+SIGN_DATA = {
+    '_client_type': '2',
+    '_client_version': '9.7.8.0',
+    '_phone_imei': '000000000000000',
+    'model': 'MI+5',
+    "net_type": "1",
+}
+
+# VARIABLE NAME
+COOKIE = "Cookie"
+BDUSS = "BDUSS"
+EQUAL = r'='
+EMPTY_STR = r''
+TBS = 'tbs'
+PAGE_NO = 'page_no'
+ONE = '1'
+TIMESTAMP = "timestamp"
+DATA = 'data'
+FID = 'fid'
+SIGN_KEY = 'tiebaclient!!!'
+UTF8 = "utf-8"
+SIGN = "sign"
+KW = "kw"
+
+s = requests.Session()
+
+
+def get_tbs(bduss):
+    logging.info("获取tbs开始")
+    headers = copy.copy(HEADERS)
+    headers.update({COOKIE: EMPTY_STR.join([BDUSS, EQUAL, bduss])})
+    try:
+        tbs = s.get(url=TBS_URL, headers=headers, timeout=5).json()[TBS]
+    except Exception as e:
+        logging.error("获取tbs出错", e)
+        logging.info("重新获取tbs开始")
+        tbs = s.get(url=TBS_URL, headers=headers, timeout=5).json()[TBS]
+    logging.info("重新获取tbs结束")
+    return tbs
+
+
+def get_favorite(bduss):
+    logging.info("获取关注的贴吧开始")
+    # 客户端关注的贴吧
+    returnData = {}
+    i = 1
+    data = {
+        'BDUSS': bduss,
+        '_client_type': '2',
+        '_client_id': 'wappc_1534235498291_488',
+        '_client_version': '9.7.8.0',
+        '_phone_imei': '000000000000000',
+        'from': '1008621y',
+        'page_no': '1',
+        'page_size': '200',
+        'model': 'MI+5',
+        'net_type': '1',
+        'timestamp': str(int(time.time())),
+        'vcode_tag': '11',
+    }
+    data = encodeData(data)
+    try:
+        res = s.post(url=LIKIE_URL, data=data, timeout=5).json()
+    except Exception as e:
+        logging.error("获取关注的贴吧出错", e)
+        return []
+    returnData = res
+    if 'forum_list' not in returnData:
+        returnData['forum_list'] = []
+    if res['forum_list'] == []:
+        return {'gconforum': [], 'non-gconforum': []}
+    if 'non-gconforum' not in returnData['forum_list']:
+        returnData['forum_list']['non-gconforum'] = []
+    if 'gconforum' not in returnData['forum_list']:
+        returnData['forum_list']['gconforum'] = []
+    while 'has_more' in res and res['has_more'] == '1':
+        i = i + 1
+        data = {
+            'BDUSS': bduss,
+            '_client_type': '2',
+            '_client_id': 'wappc_1534235498291_488',
+            '_client_version': '9.7.8.0',
+            '_phone_imei': '000000000000000',
+            'from': '1008621y',
+            'page_no': str(i),
+            'page_size': '200',
+            'model': 'MI+5',
+            'net_type': '1',
+            'timestamp': str(int(time.time())),
+            'vcode_tag': '11',
+        }
+        data = encodeData(data)
+        try:
+            res = s.post(url=LIKIE_URL, data=data, timeout=5).json()
+        except Exception as e:
+            logging.error("获取关注的贴吧出错", e)
+            continue
+        if 'forum_list' not in res:
+            continue
+        if 'non-gconforum' in res['forum_list']:
+            returnData['forum_list']['non-gconforum'].append(res['forum_list']['non-gconforum'])
+        if 'gconforum' in res['forum_list']:
+            returnData['forum_list']['gconforum'].append(res['forum_list']['gconforum'])
+
+    t = []
+    for i in returnData['forum_list']['non-gconforum']:
+        if isinstance(i, list):
+            for j in i:
+                if isinstance(j, list):
+                    for k in j:
+                        t.append(k)
+                else:
+                    t.append(j)
+        else:
+            t.append(i)
+    for i in returnData['forum_list']['gconforum']:
+        if isinstance(i, list):
+            for j in i:
+                if isinstance(j, list):
+                    for k in j:
+                        t.append(k)
+                else:
+                    t.append(j)
+        else:
+            t.append(i)
+    logging.info("获取关注的贴吧结束")
+    return t
+
+
+def encodeData(data):
+    s = EMPTY_STR
+    keys = data.keys()
+    for i in sorted(keys):
+        s += i + EQUAL + str(data[i])
+    sign = hashlib.md5((s + SIGN_KEY).encode(UTF8)).hexdigest().upper()
+    data.update({SIGN: str(sign)})
+    return data
+
+
+def client_sign(bduss, tbs, fid, kw):
+    # 客户端签到
+    logging.info("开始签到贴吧：", kw)
+    data = copy.copy(SIGN_DATA)
+    data.update({BDUSS: bduss, FID: fid, KW: kw, TBS: tbs, TIMESTAMP: str(int(time.time()))})
+    data = encodeData(data)
+    res = s.post(url=SIGN_URL, data=data, timeout=5).json()
+    return res
+
+
+def main():
+    b = os.environ['BDUSS'].split('#')
+    for i in b:
+        tbs = get_tbs(i)
+        for j in get_favorite(i):
+            client_sign(i, tbs, j[0], j[1])
+
+
+if __name__ == '__main__':
+    main()
